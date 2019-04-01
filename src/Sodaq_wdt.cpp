96104b76b8d96aa26dd9cbfa6f76a6762b5d3b58
Gabriel Notman <g.notman@gmail.com>
2019-04-01 15:58:46 +0200

    Added licenses to the files

--- src/Sodaq_wdt.cpp
+++ src/Sodaq_wdt.cpp
@@ -1,3 +1,34 @@
+/*
+Copyright (c) 2016-19, SODAQ
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <Arduino.h>
 #include "Sodaq_wdt.h"
 
@@ -8,7 +39,7 @@
 // see cores/startup.c for details
 // GCLK0   DFLL48M DIV1 = 48mhz
 // GCLK1   XOSC32K DIV1 = 32khz
-// GCLK2   
+// GCLK2
 // GCLK3   OSC8M   DIV1 = 8mhz
 #define WDT_GCLK    4
 
@@ -48,14 +79,14 @@ void sodaq_wdt_enable(wdt_period period)
 
   // We cannot configure the WDT if it is already in always on mode
   if (!(WDT->CTRL.reg & WDT_CTRL_ALWAYSON)) {
-    
+
     // Setup clock provider WDT_GCLK with a 32 source divider
     // GCLK_GENDIV_ID(X) specifies which GCLK we are configuring
     // GCLK_GENDIV_DIV(Y) specifies the clock prescalar / divider
-    // If GENCTRL.DIVSEL is set (see further below) the divider 
+    // If GENCTRL.DIVSEL is set (see further below) the divider
     // is 2^(Y+1). If GENCTRL.DIVSEL is 0, the divider is simply Y
     // This register has to be written in a single operation
-    GCLK->GENDIV.reg = GCLK_GENDIV_ID(WDT_GCLK) | 
+    GCLK->GENDIV.reg = GCLK_GENDIV_ID(WDT_GCLK) |
                        GCLK_GENDIV_DIV(4);
 
     // Configure the GCLK module
@@ -65,25 +96,25 @@ void sodaq_wdt_enable(wdt_period period)
     // GCLK_GENCTRL_DIVSEL, specify which prescalar mode we are using
     // Output from this module is 1khz (32khz / 32)
     // This register has to be written in a single operation.
-    GCLK->GENCTRL.reg = GCLK_GENCTRL_GENEN | 
-                        GCLK_GENCTRL_SRC_OSCULP32K | 
-                        GCLK_GENCTRL_ID(WDT_GCLK) | 
+    GCLK->GENCTRL.reg = GCLK_GENCTRL_GENEN |
+                        GCLK_GENCTRL_SRC_OSCULP32K |
+                        GCLK_GENCTRL_ID(WDT_GCLK) |
                         GCLK_GENCTRL_DIVSEL;
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
-    
+
     // Configure the WDT clock
     // GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_WDT), specify the WDT clock
     // GCLK_CLKCTRL_GEN(WDT_GCLK), specify the source from the WDT_GCLK GCLK
     // This register has to be written in a single operation
     GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_WDT) |
-                        GCLK_CLKCTRL_GEN(WDT_GCLK) | 
+                        GCLK_CLKCTRL_GEN(WDT_GCLK) |
                         GCLK_CLKCTRL_CLKEN;
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
 
     // Disable the module before configuring
     WDT->CTRL.reg &= ~WDT_CTRL_ENABLE;
     while (WDT->STATUS.reg & WDT_STATUS_SYNCBUSY);
-    
+
     // Disable windowed mode
     WDT->CTRL.reg &= ~WDT_CTRL_WEN;
     while (WDT->STATUS.reg & WDT_STATUS_SYNCBUSY);
@@ -98,16 +129,16 @@ void sodaq_wdt_enable(wdt_period period)
     // Enable the WDT module
     WDT->CTRL.reg |= WDT_CTRL_ENABLE;
     while (WDT->STATUS.reg & WDT_STATUS_SYNCBUSY);
-  
+
     // Enable early warning interrupt
     WDT->INTENSET.reg = WDT_INTENSET_EW;
-    
+
     // Enable interrupt vector for WDT
     // Priority is set to 0x03, the lowest
     NVIC_EnableIRQ(WDT_IRQn);
     NVIC_SetPriority(WDT_IRQn, 0x03);
   }
-  
+
 #endif
 }
 
@@ -123,17 +154,17 @@ void sodaq_wdt_disable()
   // Disable the WDT module
   WDT->CTRL.reg &= ~WDT_CTRL_ENABLE;
   while (WDT->STATUS.reg & WDT_STATUS_SYNCBUSY);
-  
+
   // Turn off the power to the WDT module
   PM->APBAMASK.reg &= ~PM_APBAMASK_WDT;
-  
+
 #endif
 }
 
 // Resets the WDT counter
 void sodaq_wdt_reset()
 {
-#ifdef ARDUINO_ARCH_AVR  
+#ifdef ARDUINO_ARCH_AVR
 
   // Using avr/wdt.h
   wdt_reset();
@@ -148,7 +179,7 @@ void sodaq_wdt_reset()
   while (WDT->STATUS.reg & WDT_STATUS_SYNCBUSY);
   WDT->CLEAR.reg = WDT_CLEAR_CLEAR_KEY;
   while (WDT->STATUS.reg & WDT_STATUS_SYNCBUSY);
-  
+
 #endif
 }
 
@@ -178,12 +209,12 @@ ISR(WDT_vect)
 #elif defined(ARDUINO_ARCH_SAMD)
 
 // SAMD WDT ISR
-// This handler is triggered by the early 
+// This handler is triggered by the early
 // warning interrupt
 void WDT_Handler(void)
 {
   sodaq_wdt_flag = true;
-  
+
   // Clear the early warning interrupt flag
   WDT->INTFLAG.reg = WDT_INTFLAG_EW;
 }
